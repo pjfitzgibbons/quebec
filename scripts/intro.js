ab507817b6faa66d3c6c9b8b1808112bf5f743f5
Peter Fitzgibbons <pjfitz@amazon.com>
2021-10-19 06:43:26 -0700

    Cleanup warnings (IntelliJ linter)

--- scripts/intro.js
+++ scripts/intro.js
@@ -1,45 +1,121 @@
-
-
+Vue.component('wbs-table', {
+    store: productStore,
+    computed: {
+        ...Vuex.mapGetters(['visibleTreeSort', 'treeObj'])
+    },
+    template: `
+            <table>
+                <thead>
+                    <tr>
+                        <th colspan="4" >Product List</th>
+                    </tr>
+                </thead>
+                <tbody>    
+                    <wbs-row
+                            v-for="wbs in visibleTreeSort"
+                            v-bind:item="treeObj[wbs]"
+                            v-bind:key="wbs"
+                    ></wbs-row>
+                </tbody>
+            </table>`
+})
 Vue.component('wbs-row', {
     props: ['item'],
     computed: {
-      componentType: function() {
-          return "row-" + this.item._type.toLowerCase()
-      }
+        componentType: function () {
+            return "row-" + this.item._type.toLowerCase()
+        }
     },
-    template: '<div>' +
-        // '<p>{{item}}</p>' +
-        '<cmponent :is="componentType" :item="item" ></cmponent>' +
-        '</div>'
-
+    template: `<cmponent :is="componentType" :item="item" ></cmponent>`
 })
 
-const wbsIndentCssClass = function(wbs) {
-    const level = wbs.split('.').count
-    return 'wbs-indent-' + level
+const collapsedSymbols = {
+    [collapsedStates.PLUS]: '&#10133;',
+    [collapsedStates.MINUS]: '&#10134;',
+    [collapsedStates.NONE]: ''
 }
+Vue.component('expander-icon', {
+    props: ['item'],
+    computed: {
+        ...Vuex.mapGetters(['wbsExpanded']),
+        symbol: function () {
+            return collapsedSymbols[ this.wbsExpanded(this.item.wbs) ]
+        },
+        wbsIndentClass: function () {
+            const wbsLevel = this.item.wbs.split('.').length
+            return this.item.rollup ? `wbs-expander-indent-${wbsLevel}` : `wbs-indent-${wbsLevel}`
+        },
+    },
+    methods: {
+        ...Vuex.mapActions(['toggleExpansion'])
+    },
+    template: `
+        <span class="expander" :class="wbsIndentClass" v-html="symbol" @click="toggleExpansion(item.wbs)"></span>`
+})
 
 Vue.component('row-product', {
     props: ['item'],
-    template: '<div>{{item.wbs}} Product</div>'
+    computed: {
+        ...Vuex.mapGetters(['rollupCost']),
+    },
+    template: `
+        <tr>
+            <td>IN P</td>
+            <td>{{item.wbs}}</td>
+            <td><expander-icon :item="item"></expander-icon>{{item.product}}</td>
+            <td class="right-align">{{rollupCost(item)}}</td>
+        </tr>`
+})
+
+Vue.component('row-program', {
+    props: ['item'],
+    computed: {
+        ...Vuex.mapGetters(['rollupCost']),
+    },
+    template: `
+        <tr>
+            <td>IN P</td>
+            <td>{{item.wbs}}</td>
+            
+            <td><expander-icon :item="item"></expander-icon>{{item.product}}</td>
+            <td class="right-align">{{rollupCost(item)}}</td>
+        </tr>`
 })
 
 Vue.component('row-address', {
     props: ['item'],
-    template: '<div>{{item.wbs}} Address</div>'
+    store: productStore,
+    computed: {
+        ...Vuex.mapGetters(['rollupCost']),
+    },
+    template: `
+        <tr>
+            <td>IN A</td>
+            <td>{{item.wbs}}</td>
+            <td><expander-icon :item="item"></expander-icon>{{item.address}}</td>
+            <td class="right-align">{{rollupCost(item)}}</td>
+        </tr>`
 })
 
 Vue.component('row-food', {
     props: ['item'],
-    template: '<div>{{item.wbs}} Food</div>'
+    computed: {
+        ...Vuex.mapGetters(['rollupCost']),
+    },
+    template: `
+        <tr>
+            <td>IN F</td>
+            <td>{{item.wbs}}</td>
+            <td><expander-icon :item="item"></expander-icon>{{item.food}}</td>
+            <td  class="right-align">{{rollupCost(item)}}</td>
+        </tr>`
 })
 
 
 var app = new Vue({
     el: '#container',
-    store: store,
+    store: productStore,
     computed: {
         ...Vuex.mapGetters(['treeSort', 'treeObj'])
     }
 })
-console.log(app)
--- scripts/store.js
+++ scripts/store.js
@@ -1,4 +1,3 @@
-console.log("Store.js")
 Vue.use(Vuex)
 
 // todo: random costs on Food level
@@ -9,41 +8,55 @@ Vue.use(Vuex)
 const _productIdxToId = idx => idx * 1_000_000
 const _addressIdxToId = (idx, productId) => productId + (idx * 1000)
 const _foodIdxToId = (idx, addressId) => addressId + idx
-const _wbs = indexes => indexes.join('.')
+const _wbs = indexes => [1,...indexes].join('.')
 
 const _generateProductTree = function () {
     const productRange = _randRange(5, 12)
     const treeList = []
 
+    const program = {
+        product: faker.company.catchPhrase(),
+        expanded: true
+    }
+    treeList.push({...program, _type: 'Product', wbs: '1', rollup: true})
+
     _.map(productRange, function (productIdx) {
         const product = {
             productId: _productIdxToId(productIdx),
             product: faker.commerce.productName(),
+            cost: 0,
+            expanded: true
         }
-        treeList.push({ ...product, _type: 'Product',  wbs:  _wbs([productIdx]), rollup: true } )
+        treeList.push({ ...product, _type: 'Program',  wbs:  _wbs([productIdx]), rollup: true } )
 
         const addressRange = _randRange(2, 5)
         _.map(addressRange, function (addressIdx) {
+            const _rollup = _.sample([true, false])
             const address = {
-                addressId: _addressIdxToId(addressIdx),
+                addressId: _addressIdxToId(addressIdx, product.productId),
                 address: faker.fake("{{address.streetAddress}}, {{address.city}} {{address.stateAbbr}} {{address.zipCodeByState}}"),
+                cost: (!_rollup) ? _randCost() : 0,
+                expanded: true
             }
-            treeList.push({...address, _type: 'Address', wbs:  _wbs([productIdx, addressIdx]), rollup: true })
 
-            const foodRange = _randRange(2, 5)
-            _.map(foodRange, function (foodIdx) {
-                const food = {
-                    foodId: _foodIdxToId(foodIdx),
-                    desc: faker.commerce.product()
-                }
+            treeList.push({...address, _type: 'Address', wbs:  _wbs([productIdx, addressIdx]), rollup: _rollup })
+            if (_rollup) {
+                const foodRange = _randRange(2, 5)
+                _.map(foodRange, function (foodIdx) {
+                    const food = {
+                        foodId: _foodIdxToId(foodIdx, address.addressId),
+                        food: faker.commerce.product(),
+                        cost: _randCost()
+                    }
 
-                treeList.push({
-                    wbs: _wbs([productIdx, addressIdx, foodIdx]),
-                    _type: 'Food',
-                    ...food,
-                    rollup: false
+                    treeList.push({
+                        wbs: _wbs([productIdx, addressIdx, foodIdx]),
+                        _type: 'Food',
+                        ...food,
+                        rollup: false
+                    })
                 })
-            })
+            }
         })
     })
     const treeObj = _.keyBy(treeList, item => item.wbs)
@@ -66,20 +79,71 @@ const _randRange = function (mid, upper) {
     return _.range(lower, rangeUpper)
 }
 
-const store = new Vuex.Store({
+/* _randCost - random cost values from an arbitrary range */
+const _randCost = () => _.round(_.random(1001, 300_000), -2)
+
+const currencyFmt = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 })
+
+const collapsedStates = {
+    NONE: 'NONE',
+    PLUS: 'PLUS',
+    MINUS: 'MINUS'
+}
+const productStore = new Vuex.Store({
     state: {
         count: 0,
-        ..._generateProductTree()
+        ..._generateProductTree(),
+        collapsedWbs: {}
     },
     mutations: {
         increment(state) {
             state.count++
+        },
+        toggleCollapsedWbs(state, wbs) {
+            if (state.collapsedWbs[wbs]) {
+                const { [wbs]: undefined, ...newCollapsedWbs } = state.collapsedWbs
+                state.collapsedWbs = newCollapsedWbs
+            }
+            else {
+                state.collapsedWbs = {...state.collapsedWbs, [wbs]: wbs}
+            }
+        }
+    },
+    actions: {
+        toggleExpansion({commit}, wbs) {
+            commit('toggleCollapsedWbs', wbs)
         }
     },
     getters: {
+        visibleTreeSort (state) {
+            // This RegExp will multi-match all the wbs "starting-with" each collapsedWbs
+            if (_.size(state.collapsedWbs) === 0) {
+                return state.treeSort
+            } else {
+                const wbsRx = RegExp(_.values(state.collapsedWbs).map(wbs => `^${wbs}\\.`).join('|'))
+                return state.treeSort.filter(wbs => !wbs.match(wbsRx))
+            }
+        },
         treeSort: (state) => state.treeSort,
-        treeObj: (state) => state.treeObj
+        treeObj: (state) => state.treeObj,
+        rollupCost: (state) => (item) => {
+            var cost
+            if (item.rollup) {
+                const parentRegex = RegExp(`^${item.wbs}\\.`)
+                cost = state.treeSort
+                    .filter(wbs => wbs.match(parentRegex))
+                    .reduce((cost, wbs) => cost + state.treeObj[wbs].cost, 0)
+            } else {
+                cost = item.cost
+            }
+            return currencyFmt.format(cost)
+        },
+        wbsExpanded: (state) => (wbs) => {
+            if (state.treeObj[wbs].rollup)
+                return  state.collapsedWbs[wbs] ? collapsedStates.MINUS : collapsedStates.PLUS
+            else
+                return collapsedStates.NONE
+        }
     }
 })
 
-console.log(_generateProductTree())
